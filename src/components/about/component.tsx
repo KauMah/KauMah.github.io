ba3117877850358d5edaaf30105853719af29831
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-03-09 13:18:40 -0500

    content update

--- src/components/about/component.tsx
+++ src/components/about/component.tsx
@@ -2,75 +2,75 @@ import { $white } from '../../assets/colors';
 import profile from '../../assets/img/profile.jpg';
 
 const styles = {
-    sect: {
-        background: $white,
-    },
-    container: {
-        paddingTop: '50px',
-        paddingBottom: '50px',
-        textAlign: 'center' as 'center',
-    },
-    profilePic: {
-        width: '80%',
-        borderRadius: '50%',
-        paddingBottom: '20px',
-    },
-    text: {
-        padding: '20px 20px',
-        textAlign: 'left' as 'left',
-    },
+  sect: {
+    background: $white,
+  },
+  container: {
+    paddingTop: '50px',
+    paddingBottom: '50px',
+    textAlign: 'center' as 'center',
+  },
+  profilePic: {
+    width: '80%',
+    borderRadius: '50%',
+    paddingBottom: '20px',
+  },
+  text: {
+    padding: '20px 20px',
+    textAlign: 'left' as 'left',
+  },
 };
 
 const calcAgeYears = () => {
-    let birthday = new Date(1999, 8, 10);
-    let elapsed = Date.now() - birthday.getTime();
-    return Math.floor(elapsed / 1000 / 60 / 60 / 24 / 365.25);
+  let birthday = new Date(1999, 8, 10);
+  let elapsed = Date.now() - birthday.getTime();
+  return Math.floor(elapsed / 1000 / 60 / 60 / 24 / 365.25);
 };
 
 const About = () => {
-    return (
-        <section id="about" style={styles.sect}>
-            <div className="container" style={styles.container}>
-                <h3>ABOUT ME</h3>
-                <hr />
-                <div className="row">
-                    <div className="col-md-6 col-sm-12">
-                        <img
-                            src={profile}
-                            className="img-fluid"
-                            alt=""
-                            style={styles.profilePic}
-                        />
-                    </div>
-                    <div className="col-md-6 col-sm-12" style={styles.text}>
-                        <p>
-                            I am a {calcAgeYears()} year old software engineer.
-                            Since a young age, I have been inclined toward the
-                            field of engineering starting with Lego
-                            constructions to simple Arduino based machines in
-                            high school. My other interests include running,
-                            playing the guitar and piano, going to the driving
-                            range, playing online chess, and spending time with
-                            my friends. These are some of the things that I find
-                            myself the most drawn to when I am not working or
-                            watching some kind of TV.
-                        </p>
-                        <p>
-                            In addition, I have a deep interest in self
-                            development and becoming fully in control of my time
-                            and actions through constant attention. A good
-                            portion of my recent reading has been focused on
-                            this matter in an attempt to dissipate less of my
-                            energy when it can be better utilized elsewhere. In
-                            doing this, I also intend to gain an fuller
-                            understanding of the human condition and therefore,
-                            myself.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="about" style={styles.sect}>
+      <div className="container" style={styles.container}>
+        <h3>ABOUT ME</h3>
+        <hr />
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <img
+              src={profile}
+              className="img-fluid"
+              alt=""
+              style={styles.profilePic}
+            />
+          </div>
+          <div className="col-md-6 col-sm-12" style={styles.text}>
+            <p>
+              I am a {calcAgeYears()} year old software engineer. I like
+              learning new things, skiing, chess, reading, making music, and
+              solving problems. My motivation comes stems my desire to ensure
+              that I make something amazing of my 20's and I want to live up to
+              the incredible stories I've heard from all the people who have
+              mentored me.
+            </p>
+            <p>
+              I operate on 3 major pillars: Thoughtfulness, Inquisivity, and
+              Playfulness. It is very important to me that my actions do not
+              adversely affect those around me. This can range from the words I
+              choose to omit form my vernacular to the way I drive on a highway
+              to minimize my impact on traffic. Curiosity has driven me to learn
+              all that I have in my life, and I recognize how valuable it has
+              been in motivating me to learn new things. Finally, life would be
+              unbelievably bland if everything was serious, so I think it's
+              important to have fun, even in serious contexts. If possible, I
+              choose to have fun even through the most mundane aspects of my
+              life. If there are things that have to be done, it might as well
+              be fun to do them. The hope is that these principles help me
+              acheive my goals in a way that is sustainable and constructive.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/contact/component.tsx
+++ src/components/contact/component.tsx
@@ -1,9 +1,9 @@
 import {
-    faGithub,
-    faInstagram,
-    faLinkedin,
-    faTwitter,
-    faYoutube,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
 
 import { css } from '@emotion/react';
@@ -11,103 +11,98 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { $black } from '../../assets/colors';
 
 const styles = {
-    container: {
-        paddingTop: '50px',
-        paddingBottom: '150px',
-        textAlign: 'center' as 'center',
-        fontSize: '40px',
+  container: {
+    paddingTop: '50px',
+    paddingBottom: '50px',
+    textAlign: 'center' as 'center',
+    fontSize: '40px',
+  },
+  spacer: {
+    justifyContent: 'center',
+  },
+  icon: css({
+    color: $black,
+    transition: 'color 0.3s, font-size 0.3s',
+    margin: '0 5px',
+  }),
+  github: css({
+    '&:hover': {
+      color: 'rgb(41, 45, 50)',
     },
-    spacer: {
-        justifyContent: 'center',
+  }),
+  youtube: css({
+    '&:hover': {
+      color: 'rgb(235, 51, 35)',
     },
-    icon: css({
-        color: $black,
-        transition: 'color 0.3s, font-size 0.3s',
-        margin: '0 5px',
-    }),
-    github: css({
-        '&:hover': {
-            color: 'rgb(41, 45, 50)',
-            fontSize: '60px',
-        },
-    }),
-    youtube: css({
-        '&:hover': {
-            color: 'rgb(235, 51, 35)',
-            fontSize: '60px',
-        },
-    }),
-    instagram: css({
-        '&:hover': {
-            color: '#d6249f',
-            fontSize: '60px',
-        },
-    }),
-    twitter: css({
-        '&:hover': {
-            color: 'rgb(74, 160, 235)',
-            fontSize: '60px',
-        },
-    }),
-    linkedin: css({
-        '&:hover': {
-            color: 'rgb(48, 118, 176)',
-            fontSize: '60px',
-        },
-    }),
+  }),
+  instagram: css({
+    '&:hover': {
+      color: '#d6249f',
+    },
+  }),
+  twitter: css({
+    '&:hover': {
+      color: 'rgb(74, 160, 235)',
+    },
+  }),
+  linkedin: css({
+    '&:hover': {
+      color: 'rgb(48, 118, 176)',
+    },
+  }),
 };
 
 const Contact = () => {
-    return (
-        <section id="contact">
-            <div style={styles.container} className="container">
-                <h3>GET IN TOUCH!</h3>
-                <hr />
-                <div className="row" style={styles.spacer}>
-                    <a
-                        title="Github"
-                        href="https://github.com/KauMah"
-                        css={[styles.icon, styles.github]}
-                        className="col-sm-1"
-                        key="github-button">
-                        <FontAwesomeIcon icon={faGithub} size="lg" />
-                    </a>
-                    <a
-                        title="Linkedin"
-                        href="https://www.linkedin.com/in/kaushikmahadevan/"
-                        css={[styles.icon, styles.linkedin]}
-                        className="col-sm-1"
-                        key="linkedin-button">
-                        <FontAwesomeIcon icon={faLinkedin} size="lg" />
-                    </a>
-                    <a
-                        title="Youtube"
-                        href="https://www.youtube.com/channel/UCZEyX114PjTP_r4Cku1oJaQ"
-                        css={[styles.icon, styles.youtube]}
-                        className="col-sm-1"
-                        key="youtube-button">
-                        <FontAwesomeIcon icon={faYoutube} size="lg" />
-                    </a>
-                    <a
-                        title="Instagram"
-                        href="https://www.instagram.com/kaushik_m_/"
-                        css={[styles.icon, styles.instagram]}
-                        className="col-sm-1"
-                        key="instagram-button">
-                        <FontAwesomeIcon icon={faInstagram} size="lg" />
-                    </a>
-                    <a
-                        title="Twitter"
-                        href="https://twitter.com/couch_chick"
-                        css={[styles.icon, styles.twitter]}
-                        className="col-sm-1"
-                        key="twitter-button">
-                        <FontAwesomeIcon icon={faTwitter} size="lg" />
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="contact">
+      <div style={styles.container} className="container">
+        <h3>GET IN TOUCH!</h3>
+        <hr />
+        <div className="" style={styles.spacer}>
+          <a
+            title="Github"
+            href="https://github.com/KauMah"
+            css={[styles.icon, styles.github]}
+            className="col-sm-1"
+            key="github-button">
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+          </a>
+          <a
+            title="Linkedin"
+            href="https://www.linkedin.com/in/kaushikmahadevan/"
+            css={[styles.icon, styles.linkedin]}
+            className="col-sm-1"
+            key="linkedin-button">
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          </a>
+          <a
+            title="Youtube"
+            href="https://www.youtube.com/channel/UCZEyX114PjTP_r4Cku1oJaQ"
+            css={[styles.icon, styles.youtube]}
+            className="col-sm-1"
+            key="youtube-button">
+            <FontAwesomeIcon icon={faYoutube} size="lg" />
+          </a>
+          <a
+            title="Instagram"
+            href="https://www.instagram.com/kaushik_m_/"
+            css={[styles.icon, styles.instagram]}
+            className="col-sm-1"
+            key="instagram-button">
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
+          </a>
+          <a
+            title="Twitter"
+            href="https://twitter.com/couch_chick"
+            css={[styles.icon, styles.twitter]}
+            className="col-sm-1"
+            key="twitter-button">
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
