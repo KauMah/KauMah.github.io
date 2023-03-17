45c082106e7ffcc7e84137499af037dfab65bff1
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-03-16 20:12:51 -0400

    minor addition

--- src/components/landing/component.tsx
+++ src/components/landing/component.tsx
@@ -1,30 +1,37 @@
 import { $white } from '../../assets/colors';
 
 const styles = {
-    sect: {
-        background:
-            'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(12,87,95,1) 54%, rgba(93,123,125,1) 90%)',
-        height: '100vh',
-    },
-    textContainer: {
-        padding: '0 20px',
-    },
-    mainText: {
-        color: $white,
-        fontSize: 'calc(1em + 3.5vw)',
-    },
+  sect: {
+    background:
+      'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(12,87,95,1) 54%, rgba(93,123,125,1) 90%)',
+    height: '100vh',
+  },
+  textContainer: {
+    padding: '0 20px',
+  },
+  mainText: {
+    color: $white,
+    fontSize: 'calc(1em + 3.5vw)',
+  },
+  boldital: {
+    fontWeight: 'bold' as const,
+    fontStyle: 'italic',
+  },
 };
 
 const IntroSection = () => {
-    return (
-        <section id="landing" style={styles.sect}>
-            <div
-                className="container h-100 d-flex justify-content-center align-items-center"
-                style={styles.textContainer}>
-                <h1 style={styles.mainText}>Hi, I'm Kaushik</h1>
-            </div>
-        </section>
-    );
+  return (
+    <section id="landing" style={styles.sect}>
+      <div
+        className="container h-100 d-flex justify-content-center align-items-center"
+        style={styles.textContainer}>
+        <h1 style={styles.mainText}>
+          {/* Kaushik: <span css={styles.boldital}>n.</span> Full Stack Developer */}
+          Hi, I'm Kaushik
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default IntroSection;
--- src/components/nav/component.tsx
+++ src/components/nav/component.tsx
@@ -1,8 +1,8 @@
 import {
-    $black,
-    $navButtonActive,
-    $primary,
-    $white,
+  $black,
+  $navButtonActive,
+  $primary,
+  $white,
 } from '../../assets/colors';
 
 import { css } from '@emotion/react';
@@ -13,149 +13,148 @@ import Navbar from 'react-bootstrap/Navbar';
 import { ScrollLink } from 'react-scroll';
 
 const styles = {
-    bar: {
-        background:
-            'linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 15%, rgba(255,255,255,.3) 100%)',
-        backdropFilter: 'blur(2px)',
-        WebkitBackdropFilter: 'blur(2px)',
-        // height: '72px',
-        padding: '12px 5px',
-        position: 'fixed' as 'fixed',
+  bar: css({
+    background:
+      'linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 15%, rgba(255,255,255,.3) 100%)',
+    backdropFilter: 'blur(2px)',
+    WebkitBackdropFilter: 'blur(2px)',
+    // height: '72px',
+    mask: 'linear-gradient(black 85%, transparent)',
+    padding: '12px 5px',
+    position: 'fixed' as 'fixed',
+  }),
+  brand: css({
+    color: $black,
+    textDecoration: 'none',
+    transition: 'color 0.3s',
+    marginTop: '-5px',
+    '&:hover': {
+      textDecoration: 'none',
+      color: $white,
     },
-    brand: css({
-        color: $black,
-        textDecoration: 'none',
-        transition: 'color 0.3s',
-        marginTop: '-5px',
-        '&:hover': {
-            textDecoration: 'none',
-            color: $white,
-        },
-    }),
-    navBox: {
-        marginTop: '-5px',
-        textAlign: 'right' as 'right',
+  }),
+  navBox: {
+    marginTop: '-5px',
+    textAlign: 'right' as 'right',
+  },
+  navBoxCondensed: {
+    marginTop: '10px',
+    textAlign: 'right' as 'right',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  navButton: css({
+    backgroundColor: $primary,
+    color: $black,
+    textDecoration: 'none',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s, color .3s',
+    border: '1px solid rgba(0,0,0,0)',
+    '&:hover': {
+      backgroundColor: $navButtonActive,
+      border: '1px solid rgba(50,50,50,0.3)',
+      color: $white,
     },
-    navBoxCondensed: {
-        marginTop: '10px',
-        textAlign: 'right' as 'right',
-    },
-    link: {
-        textDecoration: 'none',
-    },
-    navButton: css({
-        backgroundColor: $primary,
-        color: $black,
-        textDecoration: 'none',
-        cursor: 'pointer',
-        transition: 'background-color 0.3s, color .3s',
-        border: '1px solid rgba(0,0,0,0)',
-        '&:hover': {
-            backgroundColor: $navButtonActive,
-            border: '1px solid rgba(50,50,50,0.3)',
-            color: $white,
-        },
-    }),
-    buttonWrapper: {
-        display: 'inline-block',
-    },
-    buttonWrapperCondensed: {
-        display: 'inline-block',
-        margin: '8px 0',
-    },
+  }),
+  buttonWrapper: {
+    display: 'inline-block',
+  },
+  buttonWrapperCondensed: {
+    display: 'inline-block',
+    margin: '8px 0',
+  },
 };
 interface buttonTemp {
-    name: string;
-    href: string;
+  name: string;
+  href: string;
 }
 
 const navButtons: Array<buttonTemp> = [
-    {
-        name: 'Home',
-        href: 'landing',
-    },
-    {
-        name: 'About',
-        href: 'about',
-    },
-    {
-        name: 'Skills',
-        href: 'skills',
-    },
-    {
-        name: 'Projects',
-        href: 'projects',
-    },
-    {
-        name: 'Contact',
-        href: 'contact',
-    },
+  {
+    name: 'Home',
+    href: 'landing',
+  },
+  {
+    name: 'About',
+    href: 'about',
+  },
+  {
+    name: 'Skills',
+    href: 'skills',
+  },
+  {
+    name: 'Projects',
+    href: 'projects',
+  },
+  {
+    name: 'Contact',
+    href: 'contact',
+  },
 ];
 
 const Link = (props: any) => {
-    return <a {...props}>{props.children}</a>;
+  return <a {...props}>{props.children}</a>;
 };
 
 const CustomLink = ScrollLink(Link);
 
 const CustomNav = () => {
-    const [condensed, setCondensed] = useState(false);
-    return (
-        <Navbar
-            className="fixed-top"
-            collapseOnSelect
-            onToggle={() => {
-                condensed
-                    ? setTimeout(() => setCondensed(!condensed), 500)
-                    : setCondensed(!condensed);
-            }}
-            expand="md"
-            style={styles.bar}>
-            <Navbar.Brand>
-                <a href={'/#'} style={styles.link}>
-                    <h4 key="brand" css={styles.brand}>
-                        KAUSHIK MAHADEVAN
-                    </h4>
-                </a>
-            </Navbar.Brand>
-            <Navbar.Toggle
-                aria-controls="responsive-navbar-nav"
-                style={styles.navBox}
-            />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav
-                    className={
-                        condensed ? 'ml-auto d-flex' : 'd-flex ml-auto mr-4'
-                    }
-                    style={condensed ? styles.navBoxCondensed : styles.navBox}>
-                    {_.map(navButtons, (button: buttonTemp, index: number) => {
-                        return (
-                            <div
-                                style={
-                                    condensed
-                                        ? styles.buttonWrapperCondensed
-                                        : styles.buttonWrapper
-                                }
-                                key={`navButton${index}`}>
-                                <CustomLink
-                                    spy
-                                    isDynamic
-                                    smooth="easeInOutQuart"
-                                    duration={100}
-                                    activeClass="active"
-                                    offset={-50}
-                                    css={styles.navButton}
-                                    className="p-2 justify-content-start"
-                                    to={button.href}>
-                                    {button.name.toUpperCase()}
-                                </CustomLink>
-                            </div>
-                        );
-                    })}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+  const [condensed, setCondensed] = useState(false);
+  return (
+    <Navbar
+      className="fixed-top"
+      collapseOnSelect
+      onToggle={() => {
+        condensed
+          ? setTimeout(() => setCondensed(!condensed), 500)
+          : setCondensed(!condensed);
+      }}
+      expand="md"
+      css={styles.bar}>
+      <Navbar.Brand>
+        <a href={'/#'} style={styles.link}>
+          <h4 key="brand" css={styles.brand}>
+            KAUSHIK MAHADEVAN
+          </h4>
+        </a>
+      </Navbar.Brand>
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        style={styles.navBox}
+      />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav
+          className={condensed ? 'ml-auto d-flex' : 'd-flex ml-auto mr-4'}
+          style={condensed ? styles.navBoxCondensed : styles.navBox}>
+          {_.map(navButtons, (button: buttonTemp, index: number) => {
+            return (
+              <div
+                style={
+                  condensed
+                    ? styles.buttonWrapperCondensed
+                    : styles.buttonWrapper
+                }
+                key={`navButton${index}`}>
+                <CustomLink
+                  spy
+                  isDynamic
+                  smooth="easeInOutQuart"
+                  duration={100}
+                  activeClass="active"
+                  offset={-50}
+                  css={styles.navButton}
+                  className="p-2 justify-content-start"
+                  to={button.href}>
+                  {button.name.toUpperCase()}
+                </CustomLink>
+              </div>
+            );
+          })}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default CustomNav;
